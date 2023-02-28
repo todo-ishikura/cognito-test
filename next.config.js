4cfd72a6f396cbbbdb9ed143889975c25ab26bc2
Terumi Ishikura <ishikura@innovationplus.jp>
2023-02-28 11:57:21 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -4,3 +4,7 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,43 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css'
+// import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+
+import React from "react";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import { AppPropsWithLayout } from "types";
+import { MainLayout } from "components/layouts/MainLayout";
+import awsExports from "src/aws-exports";
+
+import "../styles/globals.css";
+import "antd/dist/antd.dark.css";
+import "@aws-amplify/ui-react/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+Amplify.configure({ ...awsExports, ssr: true });
+
+const components = {
+  Header() {
+    return <div style={{ padding: "24px 0" }}>loging header sample</div>;
+  },
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? MainLayout;
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    <Authenticator.Provider>
+      <QueryClientProvider client={queryClient}>
+        {getLayout(
+          <Authenticator hideSignUp components={components}>
+            <Component {...pageProps} />
+          </Authenticator>,
+        )}
+      </QueryClientProvider>
+    </Authenticator.Provider>
+  );
 }
